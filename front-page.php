ef31eff27cca1d3cfb3995c6bba7a44026019336
Myroslav <myroslavtp@gmail.com>
2024-06-28 17:04:13 +0300

    dev

--- front-page.php
+++ front-page.php
@@ -1,15 +1,15 @@
 <?php get_header(); ?>
 
 <section class="hero">
-    <a href="/shop" class="hero__wrapper">
+    <div class="hero__wrapper">
         <div class="wrapper">
             <div class="wrapper__heading">
                 <span>THE NEW COLLECTION</span>
                 <h1>Elegant & Luxury</h1>
-                <div class="button button-white">BROWSE COLLECTION</div>
+                <a  href="/shop" class="button button-white">BROWSE COLLECTION</a>
             </div>
         </div>
-    </a>
+</div>
 </section>
 
 <section class="standard-promo">
@@ -38,7 +38,7 @@
     <h2 class="shop-section__heading">New Arrivals</h2>
     <p class="shop-section__body">New arrivals. Updated every<br/> day. It's time to explore.</p>
     <div class="items">
-    <?php echo do_shortcode('[products limit="4" orderbyid="id" order="DESC"]'); ?>
+        <?php echo do_shortcode('[products limit="4" orderbyid="id" order="DESC"]'); ?>
     </div>
 </section>
 
@@ -48,7 +48,7 @@
             get updates about our latest arrivals
             and exclusive sales.</h1>
 
-            <form class="form" action="https://raddy.us20.list-manage.com/subscribe/post?u=c12aafb941c4276911b0d5cd3&amp;id=839b42c955" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate="">
+            <form class="form" action="#" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate="">
                 <input class="form__email" type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required="">
                     <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
                 <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_c12aafb941c4276911b0d5cd3_839b42c955" tabindex="-1" value=""></div>
