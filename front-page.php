de0f7a1729d77904339ac919bd6c669460b1f95b
Myroslav <myroslavtp@gmail.com>
2024-06-30 15:24:31 +0300

    dev

--- front-page.php
+++ front-page.php
@@ -14,12 +14,12 @@
 
 <section class="standard-promo">
 
-    <a href="#" class="promo-card promo-card--gray promo-card--women">
+    <a href="/product-category/women/" class="promo-card promo-card--gray promo-card--women">
         <h2 class="promo-card__heading">Shop Women</h2>
         <p class="promo-card__body">Women's new arrivals. It's time to explore your options.</p>
     </a>
 
-    <a href="#" class="promo-card promo-card--gray promo-card--men">
+    <a href="/product-category/men/" class="promo-card promo-card--gray promo-card--men">
         <h2 class="promo-card__heading">Shop Men</h2>
         <p class="promo-card__body">Mens's new arrivals. It's time to explore your options.</p>
     </a>
