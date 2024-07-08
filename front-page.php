fce78f9febd4ded6b2e993510c80422aafe470a3
Myroslav <myroslavtp@gmail.com>
2024-07-08 13:01:55 +0300

    dev

--- front-page.php
+++ front-page.php
@@ -77,12 +77,9 @@
             get updates about our latest arrivals
             and exclusive sales.</h1>
 
-            <form class="form" action="#" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate="">
-                <input class="form__email" type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required="">
-                    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-                <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_c12aafb941c4276911b0d5cd3_839b42c955" tabindex="-1" value=""></div>
-                <input class="form__submit button" type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe">
-            </form>
+            <div class="form">
+                <?php echo do_shortcode('[contact-form-7 id="a6db1ca" title="Contact form 1"]'); ?>
+            </div>
 
     </section>
 
